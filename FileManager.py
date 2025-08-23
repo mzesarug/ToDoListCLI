27eea0d12284b01ff460c855f99c3042af2c9a35
RayhanKarim1342 <rynkrmm@gmail.com>
2025-08-23 08:55:53 -0400

    added basic I/O functionality

--- FileManager.py
+++ FileManager.py
@@ -5,8 +5,8 @@ class FileManager:
     def readListFromFile(self, filePath):
         try:
             with open(filePath, 'r') as file:
-                tasks = file.readlines()
-                return [task.strip() for task in tasks if task.strip()]
+                items = file.readlines()
+                return [item.strip() for item in items if item.strip()]
         except FileNotFoundError:
             print(f"Error: The file {filePath} does not exist.")
             return []
@@ -14,12 +14,18 @@ class FileManager:
             print(f"An error occurred while reading the file: {e}")
             return []
         
-    def writeListToFile(self, filePath, tasks):
+    # Note* this will also overwrite all tasks on file, we can add an append function later if needed
+    def writeListToFile(self, filePath, items):
         try:
             with open(filePath, 'w') as file:
-                for task in tasks:
-                    file.write(f"{task}\n")
-            print(f"Tasks successfully written to {filePath}.")
+                for item in items:
+                    file.write(f"{item}\n")
         except Exception as e:
             print(f"An error occurred while writing to the file: {e}")
+
+    def clearFile(self, filePath):
+        try:
+            open(filePath, 'w').close()
+        except Exception as e:
+            print(f"An error occurred while clearing the file: {e}")
 
--- TestDriver.py
+++ TestDriver.py
@@ -2,13 +2,54 @@
 from Task import Task
 from Date import Date
 from TaskList import TaskList
+from FileManager import FileManager
 
 date = Date(2023, 12, 31, 23, 59)
 date2 = Date(2024, 12, 31, 23, 59)
 date3 = Date(2022, 12, 31, 23, 59)
-print(date)
-print(date2)    
-print(date3)
+# print(date)
+# print(date2)    
+# print(date3)
+
+# task = Task(date, "Test Task", "This is a test task", "High")
+# task2 = Task(date2, "Test Task", "This is a test task", "High")
+# task3 = Task(date3, "Test Task", "This is a test task", "High")
+# print(task)
+# print(task2)
+# print(task3)
+
+# taskList = TaskList()
+# taskList.addTask(task)
+# taskList.addTask(task2)
+# taskList.addTask(task3)
+
+# print("after adding into task list, printing list")
+# for task in taskList.getTasks():
+#     print(task)
+
+# for task in taskList.getTasks():
+#     print(task)
+
+# taskList.removeTask(task2)
+
+# print("removing task2 with 2024 date")
+# for task in taskList.getTasks():
+#     print(task)
+
+# for task in taskList.getTasks():
+#     print(task)
+
+# print(taskList)
+
+# print("clearing task list")
+# taskList.clearTasks()
+
+# for task in taskList.getTasks():
+#     print(task)
+
+# print(taskList)
+
+print("reading writing to file test")
 
 task = Task(date, "Test Task", "This is a test task", "High")
 task2 = Task(date2, "Test Task", "This is a test task", "High")
@@ -22,28 +63,20 @@ taskList.addTask(task)
 taskList.addTask(task2)
 taskList.addTask(task3)
 
-print("after adding into task list, printing list")
-for task in taskList.getTasks():
+fileManager = FileManager()
+
+print("writing to file")
+fileManager.writeListToFile("saveFile.txt", taskList.getTasks())
+
+print("reading from file")
+tasksFromFile = fileManager.readListFromFile("saveFile.txt")
+
+for task in tasksFromFile:
     print(task)
 
-for task in taskList.getTasks():
+print("clearing file")
+fileManager.clearFile("saveFile.txt")
+
+tasksFromFile = fileManager.readListFromFile("saveFile.txt")
+for task in tasksFromFile:
     print(task)
-
-taskList.removeTask(task2)
-
-print("removing task2 with 2024 date")
-for task in taskList.getTasks():
-    print(task)
-
-for task in taskList.getTasks():
-    print(task)
-
-print(taskList)
-
-print("clearing task list")
-taskList.clearTasks()
-
-for task in taskList.getTasks():
-    print(task)
-
-print(taskList)
